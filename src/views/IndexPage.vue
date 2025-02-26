adfa91a813f6704adadc61f774fe75e0bec2d48e
brian <ay83103@gmail.com>
2025-02-26 22:35:22 +0800

    feat: dropdown complete

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -1,12 +1,17 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import Checkbox from '@/components/CheckBox.vue'
+import { ref, type Ref } from 'vue'
+import DrowndownComponent from '@/components/DrowndownComponent.vue'
+import { type IOption } from '@/interfaces/Option'
 
-const test = ref({ value: 111, isChecked: false })
+const options: Ref<Array<IOption>> = ref([
+  { id: '1', name: '4', value: '11' },
+  { id: '2', name: '2', value: '22' },
+])
+const value: Ref<string> = ref('')
 </script>
 
 <template>
   <div>
-    <Checkbox v-model:value="test.value" v-model:is-checked="test.isChecked" :label="'test'" />
+    <DrowndownComponent v-model:value="value" :options="options"></DrowndownComponent>
   </div>
 </template>
