8f2e3bcf94efac8be39e518456d6b212547312d5
brian <ay83103@gmail.com>
2025-02-28 10:49:42 +0800

    feat: card complete

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card">
+    <span class="title"><slot name="title">456</slot></span>
+    <span class="sub-title"
+      ><slot name="completed-substasks"></slot> of
+      <slot name="totle-substasks"></slot> substasks</span
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  border-radius: 8px;
+  box-shadow: 0px 4px 6px rgba(#364e7e, 0.1);
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding-left: 16px;
+  padding-top: 24px;
+  padding-bottom: 24px;
+
+  .title {
+    @extend %heading-m;
+  }
+
+  .sub-title {
+    @extend %body-m;
+    color: $medium-grey;
+  }
+}
+</style>
--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -2,16 +2,18 @@
 import { ref, type Ref } from 'vue'
 import DrowndownComponent from '@/components/DrowndownComponent.vue'
 import { type IOption } from '@/interfaces/Option'
+import CardComponent from '@/components/CardComponent.vue'
 
-const options: Ref<Array<IOption>> = ref([
-  { id: '1', name: '4', value: '11' },
-  { id: '2', name: '2', value: '22' },
-])
-const value: Ref<string> = ref('')
+// const options: Ref<Array<IOption>> = ref([
+//   { id: '1', name: '4', value: '11' },
+//   { id: '2', name: '2', value: '22' },
+// ])
+// const value: Ref<string> = ref('')
 </script>
 
 <template>
   <div>
-    <DrowndownComponent v-model:value="value" :options="options"></DrowndownComponent>
+    <!-- <DrowndownComponent v-model:value="value" :options="options"></DrowndownComponent> -->
+    <CardComponent></CardComponent>
   </div>
 </template>
